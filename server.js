5311d72136f7e78fd5d351e69780b967d9a77112
InumberX <inumberx@gmail.com>
2024-10-20 16:09:14 +0900

    fix:server.js

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-import { createRequestHandler } from '@remix-run/express'
+import remix from '@remix-run/express'
 import express from 'express'
-
-// notice that the result of `remix vite:build` is "just a module"
-import * as build from './build/server/index.js'
+import * as build from './index.js'
 
 const app = express()
-app.use(express.static('build/client'))
+const port = 3000
 
-// and your app is "just a request handler"
-app.all('*', createRequestHandler({ build }))
+app.all(
+  '*',
+  remix.createRequestHandler({
+    build,
+  })
+)
 
-app.listen(3000, () => {
-  console.log('App listening on http://localhost:3000')
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })

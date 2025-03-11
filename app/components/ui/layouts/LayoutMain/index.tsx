4fcd83de7d23ee3c80d84e8b3c223cce5af846df
NiNE <35207269+InumberX@users.noreply.github.com>
2025-03-12 00:30:25 +0900

    upgrade library (#22)
    * upgrade library
    
    * fix:git workflow
    
    * fix:test

--- app/components/ui/layouts/LayoutMain/index.tsx
+++ app/components/ui/layouts/LayoutMain/index.tsx
@@ -1,10 +1,7 @@
-import { type ReactNode } from 'react'
+import { type ReactNode, type JSX } from 'react'
 import * as styles from './style.css'
 
-type Props = {
-  children?: ReactNode
-  tag?: keyof JSX.IntrinsicElements
-}
+type Props = { children?: ReactNode; tag?: keyof JSX.IntrinsicElements }
 
 export const LayoutMain = ({ children, tag }: Props) => {
   const Tag = tag ?? 'main'

9fd749c104d1ff9c53dcd3939e2cfcf84527ec07
InumberX <inumberx@gmail.com>
2025-12-17 21:29:27 +0900

    fix: base button

--- app/components/ui/buttons/BaseButton/index.tsx
+++ app/components/ui/buttons/BaseButton/index.tsx
@@ -36,11 +36,7 @@ export const BaseButton = ({
   }, [isDisabled, className, size, variant, color])
 
   return (
-    <PrimitiveButton
-      {...props}
-      className={[baseButtonClassName, className].filter(Boolean).join(' ')}
-      isDisabled={isDisabled}
-    >
+    <PrimitiveButton {...props} className={baseButtonClassName} isDisabled={isDisabled}>
       <span className={styles.baseButton_container}>
         {leftElm && leftElm}
         <span className={styles.baseButton_text}>{children}</span>

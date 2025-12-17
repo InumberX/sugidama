7d930d7f696a5a57342e6974b5200f5e691369c8
InumberX <inumberx@gmail.com>
2025-12-17 20:46:07 +0900

    fix: stories

--- stories/components/ui/layouts/LayoutPageWrapper/index.stories.tsx
+++ stories/components/ui/layouts/LayoutPageWrapper/index.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react-vite'
 import { LayoutPageWrapper } from '~/components/ui/layouts/LayoutPageWrapper'
 
 const meta: Meta<typeof LayoutPageWrapper> = {

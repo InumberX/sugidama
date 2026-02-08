c1128fe6a70df22ec03c7ac6b2fcc54c9d87be8d
InumberX <inumberx@gmail.com>
2026-02-08 18:35:15 +0900

    fix: unused i18n

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,6 +1,5 @@
 import i18next from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
-import Backend from 'i18next-http-backend'
 import { startTransition, StrictMode } from 'react'
 import { hydrateRoot } from 'react-dom/client'
 import { I18nextProvider, initReactI18next } from 'react-i18next'
@@ -11,29 +10,13 @@ import { i18n } from '~/i18n'
 
 async function hydrate() {
   await i18next
-    // Tell i18next to use the react-i18next plugin
     .use(initReactI18next)
-    // Setup a client-side language detector
     .use(LanguageDetector)
-    // Setup your backend
-    .use(Backend)
     .init({
-      // spread the configuration
       ...i18n,
-      // This function detects the namespaces your routes rendered while SSR use
       ns: getInitialNamespaces(),
-      /*
-      backend: {
-        loadPath: '/assets/locales/{{lng}}/{{ns}}.json',
-      },
-      */
       detection: {
-        // Here only enable htmlTag detection, we'll detect the language only
-        // server-side with remix-i18next, by using the `<html lang>` attribute
-        // we can communicate to the client the language detected server-side
         order: ['htmlTag'],
-        // Because we only use htmlTag, there's no reason to cache the language
-        // on the browser, so we disable it
         caches: [],
       },
     })

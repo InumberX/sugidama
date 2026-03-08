9eaa1fb1046a06ab9e55993d8d30395efdc0488f
InumberX <nine@afterworks.jp>
2026-03-08 17:18:26 +0900

    fix: LayoutPageWrapper

--- app/components/ui/layouts/LayoutPageWrapper/style.css.ts
+++ app/components/ui/layouts/LayoutPageWrapper/style.css.ts
@@ -1,9 +1,14 @@
 import { style } from '@vanilla-extract/css'
 
+import { layoutHeaderHeight } from '~/components/common/LayoutHeader/style.css'
+import { getClampPx } from '~/styles/mixins/size.css'
 import { cssLayerComponentUiLow } from '~/styles/variables/layers.css'
 
 export const layoutPageWrapper = style({
   '@layer': {
-    [cssLayerComponentUiLow]: {},
+    [cssLayerComponentUiLow]: {
+      paddingInline: 0,
+      paddingBlock: getClampPx(layoutHeaderHeight.minimum, layoutHeaderHeight.maximum),
+    },
   },
 })

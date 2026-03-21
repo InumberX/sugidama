4f7c9e53e8fa701ccc210a98158b39d04f4651c5
InumberX <nine@afterworks.jp>
2026-03-21 16:28:07 +0900

    add: FormSet

---
+++ stories/components/ui/forms/FormSet/index.stories.tsx
@@ -0,0 +1,59 @@
+import { FormSet } from '~/components/ui/forms/FormSet'
+
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+const meta: Meta<typeof FormSet> = {
+  title: 'components/ui/forms/FormSet',
+  component: FormSet,
+}
+export default meta
+
+type Story = StoryObj<typeof FormSet>
+
+export const Default: Story = {
+  args: {
+    title: '入力項目名が入ります。',
+    children: 'コンテンツが入ります。',
+  },
+}
+
+export const Required: Story = {
+  args: {
+    title: '入力項目名が入ります。',
+    isRequired: true,
+    children: 'コンテンツが入ります。',
+  },
+}
+
+export const SubTitle: Story = {
+  args: {
+    title: '入力項目名が入ります。',
+    subTitle: '補足情報が入ります。',
+    children: 'コンテンツが入ります。',
+  },
+}
+
+export const RequiredSubTitle: Story = {
+  args: {
+    title: '入力項目名が入ります。',
+    subTitle: '補足情報が入ります。',
+    isRequired: true,
+    children: 'コンテンツが入ります。',
+  },
+}
+
+export const First: Story = {
+  args: {
+    title: '入力項目名が入ります。',
+    isFirst: true,
+    children: 'コンテンツが入ります。',
+  },
+}
+
+export const Last: Story = {
+  args: {
+    title: '入力項目名が入ります。',
+    isLast: true,
+    children: 'コンテンツが入ります。',
+  },
+}

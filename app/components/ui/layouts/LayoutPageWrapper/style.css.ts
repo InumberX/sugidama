9582d20716469cec1b310c245089fd932f1ac70f
InumberX <nine@afterworks.jp>
2026-04-04 16:10:50 +0900

    fix: LayoutPageWrapper

--- app/components/ui/layouts/LayoutPageWrapper/style.css.ts
+++ app/components/ui/layouts/LayoutPageWrapper/style.css.ts
@@ -4,11 +4,33 @@ import { layoutHeaderHeight } from '~/components/common/LayoutHeader/style.css'
 import { getClampPx } from '~/styles/mixins/size.css'
 import { cssLayerComponentUiLow } from '~/styles/variables/layers.css'
 
+export const layoutPageWrapper__topNoSpace = style({
+  '@layer': {
+    [cssLayerComponentUiLow]: {},
+  },
+})
+
+export const layoutPageWrapper__bottomNoSpace = style({
+  '@layer': {
+    [cssLayerComponentUiLow]: {},
+  },
+})
+
 export const layoutPageWrapper = style({
   '@layer': {
     [cssLayerComponentUiLow]: {
       paddingInline: 0,
       paddingBlock: getClampPx(layoutHeaderHeight.minimum, layoutHeaderHeight.maximum),
+
+      selectors: {
+        [`&${layoutPageWrapper__topNoSpace}`]: {
+          paddingBlockStart: 0,
+        },
+
+        [`&${layoutPageWrapper__bottomNoSpace}`]: {
+          paddingBlockEnd: 0,
+        },
+      },
     },
   },
 })
